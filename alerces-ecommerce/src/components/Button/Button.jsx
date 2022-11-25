bb15058a24a92e79be3c0818773222a56cdf5d97
Alejandro <alebergagna0@gmail.com>
2022-11-25 15:45:47 -0300

    Creando componentes

---
+++ alerces-ecommerce/src/components/Button/Button.jsx
@@ -0,0 +1,18 @@
+import "./Button.css"
+
+function Button (){
+    // const buttonStyles = {
+    //     padding : "1rem",
+    //     backgroundColor: "yellow",
+    // }
+
+
+
+return (
+    <button className = "mi-boton">
+        Mi Boton
+    </button>
+);
+
+};
+export default Button;
---
+++ alerces-ecommerce/src/components/Nav/Nav.jsx
@@ -0,0 +1,28 @@
+import './Nav.css';
+
+function Nav(){
+    return (
+        <nav>
+            <ul className="navbar">
+                <li>
+                    <a href="">
+                        Botellas
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        Barriles
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        Servicios
+                    </a>
+                </li>
+            </ul>
+        </nav>
+
+    );
+};
+
+export default Nav;
